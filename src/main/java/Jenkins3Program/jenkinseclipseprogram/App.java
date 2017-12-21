34bb3c04402fd4a9b315c5192682a2701e5514a9
Sandeep <gaddamsandeepreddy123@gmail.com>
2017-12-21 13:42:24 +0530

    'comiting afile

---
+++ src/main/java/Jenkins3Program/jenkinseclipseprogram/App.java
@@ -0,0 +1,13 @@
+package Jenkins3Program.jenkinseclipseprogram;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
